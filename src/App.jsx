5d98ce9d1a7eeccf73f04e5c383d07d21baa90e6
waykosabhi <102040576+waykosabhi@users.noreply.github.com>
2023-06-05 18:08:47 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import "./App.css";
+import Companies from "./components/companies/Companies";
+import Recidencies from "./components/Residencies/Recidencies";
+import Value from "./components/value/Value";
+import Contact from "./components/Contact/Contact";
+import GetStarted from "./components/GetStarted/GetStarted";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Recidencies />
+      <Value />
+      <Contact />
+      <GetStarted />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Residencies/Recidencies.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
+import "./Recidencies.css";
+import data from "../../utils/slider.json";
+import { sliderSettings } from "../../utils/Common";
+const Recidencies = () => {
+  return (
+    <section className="r-wrapper">
+      <div className="paddings innerWidth r-container">
+        <div className="r-head flexColStart">
+          <span className="orangeText">Best Choices</span>
+          <span className="primaryText">Popular Residencies</span>
+        </div>
+        <Swiper {...sliderSettings}>
+          <SliderButtons />
+          {data.map((card, i) => (
+            <SwiperSlide key={i}>
+              <div className="flexColStart r-card">
+                <img src={card.image} alt="home" />
+                <span className="secondaryText r-price">
+                  <span style={{ color: "orange" }}>₹</span>
+                  <span>{card.price}</span>
+                </span>
+                <span className="primaryText">{card.name}</span>
+                <span className="secondaryText">{card.detail}</span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Recidencies;
+
+const SliderButtons = () => {
+  const swiper = useSwiper();
+  return (
+    <div className="flexCenter r-buttons">
+      <button onClick={() => swiper.slidePrev()}>&lt;</button>
+      <button onClick={() => swiper.slideNext()}>&gt;</button>
+    </div>
+  );
+};
